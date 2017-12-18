9861dac37d31864e79cb4781dfaefdefb52269e9
Mahmoud Helal <mahmoud.helal@gmail.com>
2017-12-17 16:43:45 -0800

    init

---
+++ download.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+
+
+fs.readFile(path.join(__dirname, 'customer-data.csv'), (err, data) => {
+    if (err)
+        return console.error(err);
+
+    var csv = data.toString('utf-8');
+    var arr = csv.split(/\r?\n/);
+    var res = [];
+    arr.forEach((elmnt, ind) => {
+        if (ind == 0) { }
+        else {
+            let nod = {};
+            for (let i = 0; i < arr[0].split(',').length; i++) {
+                nod[arr[0].split(',')[i]] = elmnt.split(',')[i];
+            }
+            res[res.length] = nod;
+        }
+    });
+    fs.writeFile(path.join(__dirname, 'customer-data.json'), JSON.stringify(res), (err) => {
+        if (err)
+            return console.error(err);
+        else
+            console.log('successfuly CSV converted');
+    });
+});
